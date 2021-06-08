303aa89ad3012830ecc63af57e9bdd5fa7508c94
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-09 01:03:57 +0700

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -340,7 +340,7 @@ def errors_handler(func):
 
             text = "**USERBOT CRASH REPORT**\n\n"
 
-            link = "[here](https://t.me/sn12384)"
+            link = "[Support](https://t.me/TEAMSquadUserbotSupport)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n"
