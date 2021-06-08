de87933c173e4f33ad523b3caa6fbcb1daf5fdff
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-09 01:01:13 +0700

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -45,7 +45,7 @@ def load_module(shortname):
         import userbot.utils
 
         path = Path(f"userbot/modules/{shortname}.py")
-        name = "userbot.plugins.{}".format(shortname)
+        name = "userbot.modules.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
